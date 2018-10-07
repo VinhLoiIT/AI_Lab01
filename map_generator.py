321ecd27bc2d7c45c08174dbaa639bd4d85c34e3
Loi Ly <VinhLoiIT@users.noreply.github.com>
2018-10-07 14:02:00 +0700

    Fix generate start, goal on obstacle

--- map_generator.py
+++ map_generator.py
@@ -16,10 +16,10 @@
 import random as r
 
 # Change file name here
-filename = 'input_3.txt'
+filename = 'input_4.txt'
 
 # Change size of the map here
-n = 25
+n = 10
 
 # Open File
 file = open(filename, mode='w')
@@ -27,21 +27,36 @@ file = open(filename, mode='w')
 # Write down size of the map
 print(n,file=file)
 
+# Random obstacle 
+map = []
+for y in range(n):
+    row = []
+    for x in range(n):
+        x = r.randint(0,1)
+        row.append(x)
+    map.append(row)    
+
 # Random start position
-start_x = r.randint(0, n-1)
-start_y = r.randint(0, n-1)
+while True:
+    start_x = r.randint(0, n-1)
+    start_y = r.randint(0, n-1)
+    if map[start_y][start_x] == 0:
+        break
 print(start_x, start_y, file=file)
 
 # Random goal position
-goal_x = r.randint(0, n-1)
-goal_y = r.randint(0, n-1)
+while True:
+    goal_x = r.randint(0, n-1)
+    goal_y = r.randint(0, n-1)
+    if map[goal_y][goal_x] == 0 and goal_y != start_y and goal_x != start_x:
+        break        
 print(goal_x, goal_y, file=file)
 
-# Random obstacle 
-for y in range(n):
-    for x in range(n):
-        file.write('{0} '.format(r.randint(0,1)))
+# Write down matrix
+for row in map:
+    for x in row:
+        file.write('{0} '.format(x))
     print('',file=file)
-    
+
 # Close file - Done
 file.close()
